73a68bbbdcddfe6206f688462fe96b3426974ce4
Luca Bazzea <luca.bazzea@gmail.com>
2023-12-15 20:59:29 +0200

    User model

---
+++ model/model.go
@@ -0,0 +1,9 @@
+package model
+
+type User struct {
+  ID          uint64 `json:"id" gorm:"primary_key"` 
+  Username    string `json:"username" gorm:"not null"`
+  Email       string `json:"email" gorm:"not null"` 
+  Lat         string `json:"lat"`
+  Lon         string `json:"lon"`
+}
