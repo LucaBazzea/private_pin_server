3f331ec99955a1df88d6c8da04a7c1f7ffe9c593
Luca Bazzea <luca.bazzea@gmail.com>
2024-02-09 19:44:38 +0200

    Migrations

--- database/database.go
+++ database/database.go
@@ -2,9 +2,13 @@ package database
 
 import (
 	"log"
+	"os"
 
+	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
+
+	"private_pin_server/model"
 )
 
 type DbInstance struct {
@@ -14,13 +18,18 @@ type DbInstance struct {
 var Database DbInstance
 
 func ConnectDb() {
-  db, error : gorm.Open(sqlite.Open("private_pin.db"), &gorm.Config{})
+	db, error := gorm.Open(sqlite.Open("private_pin.db"), &gorm.Config{})
 
-  if error != nil {
-    log.Fatal("DB Connection Failed \n", err.Error())
-    os.Exit(2)
-  }
+	if error != nil {
+		log.Fatal("DB Connection Failed \n", error.Error())
+		os.Exit(2)
+	}
 
-  log.Println("DB Connection Success")
-  db.Logger = logger.Default.LogMode(logger.Info)
+	log.Println("DB Connection Success")
+	db.Logger = logger.Default.LogMode(logger.Info)
+
+	log.Println("Running Migrations")
+	db.AutoMigrate(&models.User{})
+
+	Database = DbInstance{Db: db}
 }
