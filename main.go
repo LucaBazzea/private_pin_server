212953b61feefe7bd9a9ab2bb416ac4ea5978156
Luca Bazzea <luca.bazzea@gmail.com>
2024-02-09 19:09:58 +0200

    Fiber v2

--- main.go
+++ main.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-  "github.com/gofiber/fiber"
+	"github.com/gofiber/fiber/v2"
 )
 
-func hello_world(context *fiber.Ctx) {
-  context.Send("Hello World")
+func hello_world(context *fiber.Ctx) error {
+	return context.SendString("Hello World")
 }
 
 func main() {
-  app := fiber.New()
+	app := fiber.New()
 
-  app.Get("/", hello_world)
+	app.Get("/", hello_world)
 
-  app.Listen(3000)
+	app.Listen(":3000")
 }
