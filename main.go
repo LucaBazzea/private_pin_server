23f52a4b2c9b793d0544b456af699f3bb1f4c1ee
luca <auth@luca.cx>
2024-05-06 18:13:38 +0200

    user_location endpoint with dummy data

--- main.go
+++ main.go
@@ -10,11 +10,23 @@ func hello_world(context *fiber.Ctx) error {
 	return context.SendString("Hello World")
 }
 
+func user_location(context *fiber.Ctx) error {
+	user := make(map[string]interface{})
+	user["id"] = 42
+	user["name"] = "Emanuel"
+	user["lat"] = 45.47546505
+	user["lon"] = 9.17875954
+
+	return context.JSON(user)
+}
+
 func main() {
 	database.ConnectDb()
 	app := fiber.New()
 
 	app.Get("/", hello_world)
+
+	app.Get("/user_location", user_location)
 
 	app.Listen(":3000")
 }
