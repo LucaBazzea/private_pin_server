5dc69c97d35a7f6ffa0801cc40e7da46e08bec54
luca <auth@luca.cx>
2024-09-04 19:16:13 +0200

    User model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=32) # TODO: limit which characters can be used
+    email = models.EmailField()
+    lat = models.FloatField(null=True) # TODO: PGP
+    lon = models.FloatField(null=True) # TODO: PGP
+    last_online = models.DateTimeField(null=True) # TODO: PGP
+    created_at = models.DateTimeField(auto_now_add=True)
