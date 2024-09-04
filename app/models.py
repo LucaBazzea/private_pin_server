a5c48e1d80c2459c8ff2b59a0a91127467d65254
luca <auth@luca.cx>
2024-09-04 21:04:33 +0200

    Circle models

--- app/models.py
+++ app/models.py
@@ -8,3 +8,15 @@ class User(models.Model):
     lon = models.FloatField(null=True) # TODO: PGP
     last_online = models.DateTimeField(null=True) # TODO: PGP
     created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Circle(models.Model):
+    name = models.CharField(max_length=32)
+    owner = models.ForeignKey(User, on_delete=models.PROTECT)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class CircleMember(models.Model):
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    circle = models.ForeignKey(Circle, on_delete=models.PROTECT)
+    created_at = models.DateTimeField(auto_now_add=True)
